16ceb9507aeb3e10ccc0e6da118ca5f4bdd24de4
tarasowski <dimitri+github@tarasowski.de>
2025-01-13 10:46:39 +0100

    feat: added subtract

--- calculator.test.js
+++ calculator.test.js
@@ -1,10 +1,17 @@
 import assert from "assert"
-import { add, multiply } from "./calculator.js"
+import { add, multiply, subtract } from "./calculator.js"
 
 
 {
   const want = 4
   const got = add(2,2)
+
+  assert.equal(got, want)
+}
+
+{
+  const want = 2
+  const got = subtract(4,2)
 
   assert.equal(got, want)
 }
